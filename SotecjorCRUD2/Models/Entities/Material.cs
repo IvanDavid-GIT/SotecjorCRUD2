78119bff2dfbbdc1dc2fdb7e3d1cc7c375d51a96
Ivan David Martinez Torres <idmartinez58@misena.edu.co>
2021-03-18 12:56:18 -0500

    Se agrega la clase Material

---
+++ SotecjorCRUD2/Models/Entities/Material.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SotecjorCRUD2.Models.Entities
+{
+    public class Material
+    {
+        public int MaterialId { get; set; }
+        [Required(ErrorMessage ="El nombre es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El codigo del material es requerido")]
+        public int CodigoMaterial { get; set; }
+        [Required(ErrorMessage = "La categoria del material es requerida")]
+        public int CategoriaMaterial { get; set; }
+       
+        public int Cantidad { get; set; }
+    }
+}
